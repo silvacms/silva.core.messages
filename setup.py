6b5ada2e8b4d4b88afdf0c0405448283390c9f07
Antonin Amand <antonin.amand@gmail.com>
2010-06-11 16:54:04 +0000

    import

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '2.3dev'
+
+setup(name='silva.core.messages',
+      version=version,
+      description="Message utilities for Silva",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Framework :: Zope2",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: BSD License",
+        ],
+      keywords='silva core messages',
+      author='Infrae',
+      author_email='info@infrae.com',
+      url='http://infrae.com/products/silva',
+      license='BSD',
+      package_dir={'': 'src'},
+      packages=find_packages('src'),
+      namespace_packages=['silva', 'silva.core'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'zope.container',
+          'z3c.messages',
+          ],
+      )
---
+++ src/silva/core/messages/lru.py
@@ -0,0 +1,61 @@
+from BTrees.OOBTree import OOBTree
+from zope.container.interfaces import IContainer
+from zope.interface import implements
+#import OFS.SimpleItem
+import persistent.list
+import persistent.persistent
+
+
+class PersistentLRUFolder(persistent.persistent):
+    implements(IContainer)
+
+    _max_items = 1000
+    _tolerance = 0.1 # 10%
+
+    def __init__(self, max_items):
+        self._storage = OOBTree()
+        self._index = persistent.list()
+        self.set_max_items(max_items)
+
+    def set_max_items(self, val):
+        self._max_items = val
+
+    def set_tolerance(self, val):
+        self._tolerance = val
+
+    def __len__(self):
+        return len(self._index)
+
+    def get(self, key, default=None):
+        return self._storage.get(key, default)
+
+    def __getitem__(self, key):
+        return self._storage[key]
+
+    def __contains__(self, key):
+        return key in self._storage
+
+    def __setitem__(self, key, value):
+        self._storage[key] = value
+        self._index.append(key)
+        ratio = len(self.index) - self._max_items) / self._max_items
+        if ratio > self._tolerance:
+            self.truncate()
+
+    def __delitem__(self, key):
+        del self._storage[key]
+
+    def truncate(self):
+        exceed = self.max_items - len(self._index)
+        if exceed <= 0:
+            return 0
+
+        delete_ids = self._index[:exceed]
+        for id in delele_ids:
+            try:
+                del self._storage[id]
+            except KeyError:
+                continue
+        del self._index[:exceed]
+
+        return exceed
