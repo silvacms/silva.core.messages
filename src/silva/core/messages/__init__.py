017c30517d944c3ed60685989b8571a6f66a5744
Antonin Amand <antonin.amand@gmail.com>
2010-06-29 12:04:11 +0000

    add cleanup to register utility for testing

--- src/silva/core/messages/__init__.py
+++ src/silva/core/messages/__init__.py
@@ -1,7 +1,13 @@
 from zope.component import provideUtility
 from silva.core.messages.service import MemoryMessageService
 from silva.core.messages.interfaces import IMessageService
+from zope.testing import cleanup
+
 
 # XXX : fix this to use configuration to load the service
-provideUtility(MemoryMessageService(), IMessageService)
 
+def register_utility():
+    provideUtility(MemoryMessageService(), IMessageService)
+
+register_utility()
+cleanup.addCleanUp(register_utility)
